2f5f391be85ed4270cd7682813d2223aac149ae9
YuyaItabashi3594 <asuholover@gmail.com>
2024-03-14 11:40:11 +0900

    常に2桁になるように修正

--- utils/getRemainingTime.ts
+++ utils/getRemainingTime.ts
@@ -8,5 +8,5 @@ export const getRemainingTime = (expiry: Date) => {
   const minute = Math.floor((diff - hour * 60 * 60 * 1000) / 60 / 1000) % 60;
   const second =
     Math.floor((diff - hour * 60 * 60 * 1000 - minute * 60 * 1000) / 1000) % 60;
-  return `${hour}:${minute}:${second}`;
+  return `${hour}:${minute<10?'0'+minute:minute}:${second<10?'0'+second:second}`;
 };
